224b8cff4c161e42d64b54bc5bd4e7db2f7932d2
Rafael Azevedo Nascimento <rafaelnascimento@MacBook-Pro-de-eldorado.local>
2023-01-11 17:42:28 -0300

    hello word

---
+++ src/main/java/br/com/dea/management/management/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package br.com.dea.management.management.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping(value = "/Hello-World/", method = RequestMethod.GET)
+    public ResponseEntity<String> getFactory() {
+        return ResponseEntity.ok( "helloworld");
+    }
+}
+
+
